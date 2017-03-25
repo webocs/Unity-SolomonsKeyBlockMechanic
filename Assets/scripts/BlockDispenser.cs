30f03f2919f5ef1eee53a53edbfbb6ce316c6521
Maxi <maximiliano.arias@fai.uncoma.edu.ar>
2017-03-25 00:39:52 -0300

    Uploading full project

---
+++ Assets/scripts/BlockDispenser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDispenser : MonoBehaviour {
+    public GameObject block;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //   gameObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(transform.position), Vector2.zero);
+            if(hit.collider == null)
+                if (block)
+                {
+                    Vector2 position = new Vector2(Mathf.RoundToInt(gameObject.transform.position.x), Mathf.RoundToInt(gameObject.transform.position.y));
+                    Instantiate(block, position, Quaternion.identity);
+                }
+        }
+
+    }
+}
---
+++ Assets/scripts/SideScrollController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideScrollController : MonoBehaviour {
+
+    Animator animator;
+    Rigidbody2D rb;
+    public float walkSpeed;
+    public float jumpPower;
+
+    bool grounded;
+ 
+    public Transform groundCheck;
+    public float groundRadius;
+    public LayerMask whatIsGround;
+
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        grounded = Physics2D.OverlapCircle(groundCheck.position,groundRadius,whatIsGround);
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            animator.SetTrigger("walkingLeft");
+            rb.AddForce(Vector2.left*walkSpeed);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetTrigger("walkingRight");
+            rb.AddForce(Vector2.right* walkSpeed);
+        }
+
+        else
+        {
+            animator.SetTrigger("idle");
+            rb.isKinematic = true;
+            rb.isKinematic = false;
+        }
+     
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        {           
+            rb.AddForce(Vector2.up * jumpPower);
+        }
+
+    }
+
+  
+}
